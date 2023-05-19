d01904637834a8de0cec8ff97b8f81ce52632b8c
rinkuberde <rinkuberde@gmail.com>
2023-05-20 00:57:54 +0530

    login page  UI done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './Components/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatFormFieldModule,FormsModule,MatToolbarModule,
+    MatInputModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
